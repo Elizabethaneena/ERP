ded2cbb6f50320623d5266135251b69ad4e50c98
anee <anee1234@gmail.com>
2021-08-20 13:15:27 +0530

    Updating organization

--- employee.py
+++ employee.py
@@ -1,5 +1,6 @@
-employee = {} #Empty dictionary
-groups = {} #empty dictionary
+employee = {} 
+groups = {} 
+org={}
 def add_employee():
 	serial_no = input("Enter serial_no")
 	if serial_no not in employee.keys():
@@ -32,32 +33,77 @@ def delete_employee():
 	else:
 		del employee[serial_no]
 def search_employee():
-	name = input("Enter name")
-	found = False
-	for i in employee.values():
-		if i["name"] == name:
-			print(f"{i['name']} | {i['age']} |{i['gender']} |{i['place']} |{i['salary']} | {i['previous_company']} | {i['joining_date']} ")
-			found = True
-			break
-		if found == False :
-			print("Not found")
+	#name = input("Enter name")
+	#found = False
+	#for i in employee.values():
+	#	if i["name"] == name:
+	#		print(f"{i['name']} | {i['age']} |{i['gender']} |{i['place']} |{i['salary']} | {i['previous_company']} | {i['joining_date']} ")
+	#		found = True
+	#		break
+	#	if found == False :
+	#		print("Not found")
+	print("Press 1 for search by name")
+	print("Press 2 for search by age")
+	print("Press 3 for search by salary")
+	print("Press 4 for search  by gender")
+	print("Press 5 for exit")
+	while True:
 	
+		choice = int(input("Enter choice : "))
+		if choice == 1:
+			search_by_name()
+		elif choice == 2:
+			search_by_age()
+		elif choice == 3:
+			search_by_salary()
+		elif choice == 4:
+			search_by_gender()
+		elif choice == 5:
+			break
+		else:
+			print("Invalid choice")
+def search_by_name():	
+	name=input("Enter the name: ")
+	print(list(filter(lambda a:a["name"] == name, employees.values())))
+	print("Employee founded")
+
+def search_by_age():	
+	age=int(input("Enter the age of employee : "))
+	print(list(filter(lambda a:a["age"] == age, employees.values())))
+	print("Employee founded")
+
+def search_by_salary():	
+	salary=int(input("Enter the salary of employee: "))
+	print(list(filter(lambda a:a["salary"] == salary, employees.values())))
+	print("Employee founded")
+
+def search_by_gender():	
+	gender=input("Enter the gender of employee : ")
+	print(list(filter(lambda a:a["gender"] == gender, employees.values())))
+	print("Employee founded")
+
 
 def display_employee():
 	for serial,employee in employee.items():
 		print(f"{serial} | {employee['name']} | {employee['age']} | {employee['gender']} | {employee['place']} | {employee['salary']} | {employee['previous_company']} | {employee['joining_date']}")
 
-
-def change_employee_name():
+def change_employee_details():
+	print("1.Change employee name")
+	print("2.Change employee age")
+	print("3.Change employee gender")
+	print("4.Change employee salary")
+	ch =int(input("\tEnter your choice : "))
 	serial_no = input("\tEnter serial no.")
-	if serial_no not in employee.keys():
-		print("\tWrong serial no")
-	else:
+	if cho == 1:
 		employee[serial_no]['name'] = input("\tEnter new name")
+	elif ch == 2:
 		employee[serial_no]['age'] = int(input("Enter the new age"))
+	elif ch == 3:
 		employee[serial_no]['gender'] = input("Enter the new gender")
+	elif ch == 4:
 		employee[serial_no]['place'] = input("Enter the new place")
-
+	else:
+		print("Invalid choice")
 
 def main_menu():
 	print("1. Add employee details")
@@ -111,7 +157,7 @@ def delete_group():
 		print("\tWrong group name")
 
 def display_groups():
-	for key,value in groups.items(): #key is group_name,value is list of employee
+	for key,value in groups.items(): 
 		name_string = ""
 		for i in value:
 			name_string = name_string +"|"+employee[i]["name"]
@@ -141,9 +187,9 @@ def manage_group():
 		print("\tInvalid choice")	
 	
 def add_member(group_name):
-	display_student()
+	display_employee()
 	serial_no = input("\t\tEnter the serial no of student ")
-	if serial_no in students.keys():
+	if serial_no in employee.keys():
 		groups[group_name].append(serial_no)
 	else:
 		print("\t\tWrong serial No.")
@@ -151,7 +197,7 @@ def add_member(group_name):
 def list_member(group_name):
 	name_string=""
 	for i in groups[group_name]:
-		name_string = name_string +"|"+i+"."+students[i]["name"]
+		name_string = name_string +"|"+i+"."+employee[i]["name"]
 	print(f"{name_string}")
 
 def delete_member(group_name):
@@ -162,11 +208,48 @@ def delete_member(group_name):
 	else:
 		print("\t\tWrong serial No.")
 
-	
+def display_member(team_name):
+	name_string=""
+	for i in teams[team_name]:
+		name_string=name_string +"|"+i+"."+employee[i]["name"]
+	print(f"{name_string}")
 
+def add_oganization():
+	org['name']=input("Enter organization name")
+	org['email']=input("Enter organization  email")
 
+def edit_oganization():
+	print("1.Edit Organization name ")
+	print("2.Edit Organization email")
+	choice = int(input("Enter choice: "))
+	if choice == 1:
+		org['name'] = input("Enter new organization name")
+	elif choice == 2:
+		org['email'] = input("Enter new organization email")	
+	else:
+		print("Invalid choice")
+def display_organization():
+	print(org)
+def org_details():
+	print("1.Add Organization")
+	print("2.Edit Organization")
+	print("3.Display Organization details")
+	print("4.Exit")
+	while True:
+		choice = int(input("Enter choice : "))
+		if choice == 1:
+			add_oganization()
+		elif choice == 2:
+			edit_oganization()
+		elif choice == 3:
+			display_organization()
+		elif choice == 4:
+			break
+		else:
+			print("Invalid choice")	
 
 while True:
+	org_details()
 	main_menu()
 	ch = int(input("Enter your choice"))
 	if ch == 1:
@@ -182,8 +265,8 @@ while True:
 		#Display 
 		display_employee()
 	elif ch== 5:
-		#Change a employee name in the list
-		change_employee_name()
+		#Change 
+		change_employee_details()
 	elif ch == 6:
 		#Manage groups
 		manage_all_groups()
@@ -192,5 +275,4 @@ while True:
 		break;
 	else:
 		print("Invalid Choice")
-
 
